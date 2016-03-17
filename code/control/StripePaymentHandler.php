492e30c754b3df168f84494341cb8183420a7502
Morven Lewis-Everley <morven@i-lateral.com>
2016-03-17 17:05:10 +0000

    Add basic integration and config settings

--- code/control/StripePaymentHandler.php
+++ code/control/StripePaymentHandler.php
@@ -1,11 +1,79 @@
 <?php
 
+use \Stripe\Stripe as Stripe;
+use \Stripe\Charge as StripeCharge;
+
 class StripePaymentHandler extends PaymentHandler
 {
 
     public function index($request)
     {
+        $site = SiteConfig::current_site_config();
+        $order = $this->getOrderData();
+        $cart = ShoppingCart::get();
+        
+        // Setup stripe keys
+        if(Director::isDev()) {
+            $secret_key = StripePayment::config()->test_secret_key;
+            $publish_key = StripePayment::config()->test_publish_key;
+        } else {
+            $secret_key = StripePayment::config()->live_secret_key;
+            $publish_key = StripePayment::config()->live_publish_key;
+        }
+        
+        $callback_url = Controller::join_links(
+            Director::absoluteBaseURL(),
+            Payment_Controller::config()->url_segment,
+            "callback",
+            $this->payment_gateway->ID
+        );
+        
+        Stripe::setApiKey($secret_key);
+        
         $this->extend('onBeforeIndex');
+
+        // Setup stripe JS call for checkout
+        $html = '<script ';
+        $html .= 'src="https://checkout.stripe.com/checkout.js" ';
+        $html .= 'class="stripe-button" ';
+        $html .= 'data-key="' . $publish_key . '" ';
+        $html .= 'data-name="' . $site->Title . '" ';
+        $html .= 'data-email="' . $order->Email . '" ';
+        
+        if(StripePayment::config()->remember_me) {
+            $html .= 'data-allow-remember-me="true" ';
+        } else {
+            $html .= 'data-allow-remember-me="false" ';
+        }
+        
+        $html .= 'data-amount="' . (string)(round($cart->TotalCost * 100)) . '" ';
+        $html .= 'data-currency="' . strtolower(Checkout::config()->currency_code) . '" ';
+        $html .= 'data-locale="auto" ';
+        $html .= '></script>';
+        
+        $form = Form::create(
+            $this,
+            "Form",
+            FieldList::create(
+                LiteralField::create("StripeJS", $html)
+            ),
+            FieldList::create()
+        );
+        
+        $form
+            ->addExtraClass('forms')
+            ->setFormMethod('POST')
+            ->setFormAction($callback_url)
+            ->disableSecurityToken();
+            
+        Session::set("StripePayment.OrderNumber", $order->OrderNumber);
+        
+        $this->customise(array(
+            "Title"     => _t('Checkout.Summary', "Summary"),
+            "MetaTitle" => _t('Checkout.Summary', "Summary"),
+            "Form"      => $form,
+            "Order"     => $order
+        ));
         
         $this->extend("onAfterIndex");
         
@@ -22,12 +90,78 @@ class StripePaymentHandler extends PaymentHandler
      */
     public function callback($request)
     {
+        
+        if(Director::isDev()) {
+            $secret_key = StripePayment::config()->test_secret_key;
+            $publish_key = StripePayment::config()->test_publish_key;
+        } else {
+            $secret_key = StripePayment::config()->live_secret_key;
+            $publish_key = StripePayment::config()->live_publish_key;
+        }
+        
+        Stripe::setApiKey($secret_key);
+        
+        $site = SiteConfig::current_site_config();
+        $order = $this->getOrderData();
+        $cart = ShoppingCart::get();
+        
         $this->extend('onBeforeCallback');
+        
+        $error_url = Controller::join_links(
+            Director::absoluteBaseURL(),
+            Payment_Controller::config()->url_segment,
+            'complete',
+            'error'
+        );
         
         $data = $this->request->postVars();
         $status = "error";
-        $content = file_get_contents('php://input');
+
+        // Get the credit card details submitted by the form
+        $token = $data['stripeToken'];
         
-        return $this->httpError(500);
+        $order_no = Session::get("StripePayment.OrderNumber");
+        
+        $order = Order::get()
+            ->filter("OrderNumber", $order_no)
+            ->first();
+        
+        if($order_no && $order) {
+            // Create the charge on Stripe's servers - this will charge
+            // the user's card
+            try {
+                $success_url = Controller::join_links(
+                    Director::absoluteBaseURL(),
+                    Payment_Controller::config()->url_segment,
+                    'complete',
+                    $order_no
+                );
+                
+                $charge = StripeCharge::create(array(
+                    "amount" => round($cart->TotalCost * 100),
+                    "currency" => strtolower(Checkout::config()->currency_code),
+                    "source" => $token,
+                    "metadata" => array("Order" => $order_no)
+                ));
+                
+                $order->Status = "paid";
+                $order->PaymentProvider = "Stripe";
+                $order->PaymentNo = $charge->id;
+                
+                $order->write();
+                
+                return $this->redirect($success_url);
+                
+            } catch(\Stripe\Error\Card $e) {
+                $order->Status = "failed";
+                $order->write();
+                
+                return $this->redirect($error_url);
+            } finally {
+                return $this->redirect($error_url);
+            }
+        } else {
+            return $this->redirect($error_url);
+        }
     }
 }
--- code/model/StripePayment.php
+++ code/model/StripePayment.php
@@ -3,9 +3,55 @@
 class StripePayment extends PaymentMethod
 {
     
+    /**
+     * The test secret Stripe API key
+     * 
+     * @config
+     */
+    private static $test_secret_key;
+    
+    /**
+     * The test publish API key
+     * 
+     * @config
+     */
+    private static $test_publish_key;
+    
+    /**
+     * The live secret Stripe API key
+     * 
+     * @config
+     */
+    private static $live_secret_key;
+    
+    /**
+     * The live publish API key
+     * 
+     * @config
+     */
+    private static $live_publish_key;
+    
+    /**
+     * Allow "remember me" functionality (that will save user
+     * payment details)
+     * 
+     * @config
+     */
+    private static $remember_me = true;
+    
     public static $handler = "StripePaymentHandler";
 
     public $Title = 'Stripe';
+    
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        
+        $fields->removeByName("PaymentInfo");
+        $fields->removeByName("PaymentURLS");
+        
+        return $fields;
+    } 
 
     public function onBeforeWrite()
     {
@@ -13,6 +59,6 @@ class StripePayment extends PaymentMethod
 
         $this->CallBackSlug = (!$this->CallBackSlug) ? 'Stripe' : $this->CallBackSlug;
 
-        $this->Summary = (!$this->Summary) ? "Pay with Stripe" : $this->Summary;
+        $this->Summary = (!$this->Summary) ? _t("CheckoutStripe.PayWithCard","Pay with credit/debit card via Stripe") : $this->Summary;
     }
 }
